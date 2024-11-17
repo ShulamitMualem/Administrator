d29653d3603470611ae1a0a98aacd4ed021aef2a
ShulamitMualem <38328264650@mby.co.il>
2024-11-17 15:29:28 +0200

    Add project files.

---
+++ Administrator/Products.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Administrator
+{
+    internal class Products
+    {
+        public int AddNewProduct(string connectionString)
+        {
+            int rowsEffected = 0;
+            string Category_ID, Name, Description, Price, Image_Url, toContinue="";
+            while (toContinue != "n")
+            {
+                Console.WriteLine("insert Category_ID: ");
+                Category_ID = Console.ReadLine();
+                Console.WriteLine("insert Name: ");
+                Name = Console.ReadLine();
+                Console.WriteLine("insert Description: ");
+                Description = Console.ReadLine();
+                Console.WriteLine("insert Price: ");
+                Price = Console.ReadLine();
+                Console.WriteLine("insert Image_Url: ");
+                Image_Url = Console.ReadLine();
+
+                string query = "INSERT INTO Product(Category_ID, Name, Description, Price, Image_Url)" +
+                    "VALUES (@Category_ID, @Name, @Description, @Price, @Image_Url)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                    using(SqlCommand cmd=new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.Add("@Category_ID", SqlDbType.Int).Value= int.Parse(Category_ID);
+                    cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = Name;
+                    cmd.Parameters.Add("@Description", SqlDbType.NVarChar).Value = Description;
+                    cmd.Parameters.Add("@Price", SqlDbType.NVarChar).Value = Price;
+                    cmd.Parameters.Add("@Image_Url", SqlDbType.NVarChar).Value = Image_Url;
+                    connection.Open();
+                    rowsEffected += cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+                Console.WriteLine("Are you want to continue? (y/n)");
+                toContinue = Console.ReadLine();
+            }
+            return rowsEffected;
+        }
+    }
+}
---
+++ Administrator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+
+namespace Administrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dsf");
+            string connectDB = "data source=srv2\\pupils;initial catalog=Web_Api_328264650;Integrated Security=SSPI;Persist Security Info=False;TrustServerCertificate=true";
+            Products product = new Products();
+            int rowsEffected= product.AddNewProduct(connectDB);
+            Console.WriteLine("dsf");
+            Console.WriteLine($"{rowsEffected} rows are affected");
+        }
+    }
+}
